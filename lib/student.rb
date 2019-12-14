1b7804a31c08251dd8adb01b4dad0aee5376d87c
Paul Leugemors <pleugemors@yahoo.com>
2019-12-14 00:26:49 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
-
+class Student < User 
+  
+  def initialize 
+    @knowledge = []
+  end
+  
+  def learn(knowledge_str)
+    @knowledge << knowledge_str
+  end
+  
+  def knowledge
+    @knowledge 
+  end
+  
 end
